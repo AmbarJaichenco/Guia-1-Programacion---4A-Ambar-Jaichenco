c5afa3dfb55daf844678932a713e8b1574776d23
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2022-03-28 14:19:53 -0300

    Exercises 1 to 5

---
+++ Assets/Scripts/EJ5.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ5 : MonoBehaviour
+{
+    /// <summary>
+    /// Realizá un programa que, dados dos números enteros num1 y num2 cargados desde  
+    /// el Inspector muestre el resultado de la división del primero por el segundo
+    /// en la consola siguiendo el formato num1 + “ / “ + num2 + “ = “ + resultado.  
+    /// En el caso de que el segundo valor num2 sea cero, mostrar el siguiente mensaje de error:
+    /// “No se puede dividir por cero”. 
+
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num2 > 0)
+        {
+            int resultado;
+            resultado = num1 / num2;
+            Debug.Log("La división entre " + num1 + " y " + num2 + " es igual a " + resultado);
+
+        }
+
+        else
+        {
+            Debug.Log("no se puede dividir por cero");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ej2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ej2 : MonoBehaviour
+{
+    //Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,
+    //respectivamente, realice la operación correspondiente y muestre el resultado en pantalla:
+    //a.Suma
+    //b.Producto
+
+    int num1;
+    int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        num1 = 4;
+        num2 = 5;
+
+        int rtasuma;
+        int rtaproducto;
+
+        rtasuma = num1 + num2;
+        Debug.Log(rtasuma);
+
+        rtaproducto = num1 * num2;
+        Debug.Log(rtaproducto);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
