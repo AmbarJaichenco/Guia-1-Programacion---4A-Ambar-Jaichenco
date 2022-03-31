ac54fdd9b187cc4afca73c63080ced822159ad0a
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2022-03-31 10:37:12 -0300

    Excercises 5 - 9

---
+++ Assets/Scripts/EJ6.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ6 : MonoBehaviour
+{
+//    Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0
+//en una variable llamada num1 y muestre un mensaje por pantalla indicando &quot;el número es
+//par&quot; o &quot;el número es impar&quot;. Deberá utilizar el operador “módulo” es el caracter %.
+
+   public int num1; 
+
+    void Start()
+    {
+        if(num1 % 2 == 0)
+        {
+            Debug.Log("Es un número par");
+        }
+        else
+        {
+            Debug.Log("Es un numero impar");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EJ8.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ8 : MonoBehaviour
+{
+    //    Crear una variable entera que pueda ser modificada desde el Inspector.Suponiendo que
+    //el primer día de la semana es el Domingo, mostrar el nombre del día correspondiente al
+    //valor de la variable “dia”. Si el día no está en el rango permitido debe emitir por pantalla el
+    //mensaje de error "El día ingresado no es válido"
+
+    public int dia;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string nombredia = "";
+        switch (dia)
+        {
+            case 1:
+                nombredia = "Domingo";
+                break;
+            case 2:
+                nombredia = "Lunes";
+                break;
+            case 3:
+                nombredia = "Martes";
+                break;
+            case 4:
+                nombredia = "Miercoles";
+                break;
+            case 5:
+                nombredia = "Jueves";
+                break;
+            case 6:
+                nombredia = "Viernes";
+                break;
+            case 7:
+                nombredia = "Sabado";
+                break;
+            default:
+                Debug.Log("El día ingresado no es válido");
+                break;
+        }
+        Debug.Log(nombredia);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
