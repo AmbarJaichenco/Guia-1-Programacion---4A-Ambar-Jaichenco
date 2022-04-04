b802dae2c53fb4ae9d32ba4693ae1f31bb66ab33
AmbarJaichenco <ambarjazminjaichenco@gmail.com>
2022-04-04 14:20:32 -0300

    Exercices 8-10

---
+++ Assets/Scripts/EJ10.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ10 : MonoBehaviour
+{
+    //Realizá un programa que resuelva el siguiente problema: Nos piden desarrollar un
+    //programa que informe si un estacionamiento se encuentra o no abierto según la hora
+    //ingresada por el usuario.
+    //Nosotros sabemos que el estacionamiento se encuentra abierto entre las 10 y las 18hs.
+    //(Crear constante HORA_APERTURA = 10, y HORA_CIERRE = 18).
+    //Al iniciar el programa, se tomará como hora actual el valor de una variable expuesta en el
+    //Inspector, y deberemos setear una variable booleana llamada estaAbierto en true o false,
+    //según corresponda, e informar por pantalla mediante la misma si el estacionamiento se
+    //encuentra abierto o no.Obs: En el caso de que el valor de la variable correspona a una hora
+    //menor a 0 o mayor a 24, mostrar el mensaje: “Ha ingresado una hora incorrecta”
+
+    int hora_Aperturea = 10;
+    int hora_Cierre = 18;
+    public int hora_Actual = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool estaAbierto;
+
+        if (hora_Actual <0 || hora_Actual > 24)
+        {
+            Debug.Log("Ha ingresado una hora incorrecta");
+        }
+
+        else if (hora_Actual >hora_Aperturea && hora_Actual < hora_Cierre)
+        {
+            estaAbierto = true;
+            Debug.Log("El estacionamiento esta abierto");
+        }
+
+        else
+        {
+            estaAbierto = false;
+            Debug.Log("El estacionamiento no esta abierto");
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/EJ9.cs
+++ Assets/Scripts/EJ9.cs
@@ -11,12 +11,29 @@ public class EJ9 : MonoBehaviour
     //Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego
     //mostrar lo pedido en el siguiente formato:
     //Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ ….
+
     // Start is called before the first frame update
 
-    
+    public string persona1 = "";
+    public string persona2 = "";
+    public string persona3 = "";
+
+    public float cap1 = 0;
+    public float cap2 = 0;
+    public float cap3 = 0;
+    public float captotal;
+
     void Start()
     {
-        
+        captotal = cap1 + cap2 + cap3;
+        float porc1 = cap1/captotal * 100;
+        float porc2 = cap2 / captotal * 100;
+        float porc3 = cap3 / captotal * 100;
+
+        Debug.Log("Nombre: " + persona1 + "capital aportado $" + cap1 + ", Porcentaje del capital %" + porc1 + ", Monto total aportado " + captotal);
+        Debug.Log("Nombre: " + persona2 + "capital aportado $" + cap2 + ", Porcentaje del capital %" + porc2 + ", Monto total aportado " + captotal);
+        Debug.Log("Nombre: " + persona3 + "capital aportado $" + cap3 + ", Porcentaje del capital %" + porc3 + ", Monto total aportado " + captotal);
+
     }
 
     // Update is called once per frame
